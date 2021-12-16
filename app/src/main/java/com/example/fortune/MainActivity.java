23a0a238bc7ea35fee11cfac3547dbd04cd9c176
kys0226 <kyscookie@naver.com>
2021-12-16 12:23:29 +0900

    2nd commit

--- app/src/main/java/com/example/fortune/MainActivity.java
+++ app/src/main/java/com/example/fortune/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -22,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8;
-        final ViewFlipper vFlipper;
+        final LinearLayout linearLayout;
 
         btn1 = (ImageView) findViewById(R.id.fortune1);
         btn2 = (ImageView) findViewById(R.id.fortune2);
@@ -32,13 +33,21 @@ public class MainActivity extends AppCompatActivity {
         btn6 = (ImageView) findViewById(R.id.fortune6);
         btn7 = (ImageView) findViewById(R.id.fortune7);
         btn8 = (ImageView) findViewById(R.id.fortune8);
-        vFlipper = (ViewFlipper) findViewById(R.id.ViewFlipper);
+        linearLayout = (LinearLayout) findViewById(R.id.linear);
 
-        TextView text1, text2, text3, text4;
+        TextView text1, text2, text3, text4, text5;
         text1 = (TextView) findViewById(R.id.txt1);
         text2 = (TextView) findViewById(R.id.txt2);
         text3 = (TextView) findViewById(R.id.txt3);
         text4 = (TextView) findViewById(R.id.txt4);
+        text5 = (TextView) findViewById(R.id.txt5);
+
+        int a, b, c, d, e;
+        a = (int)((Math.random() * 100) + 1);
+        b = (int)((Math.random() * 100) + 1);
+        c = (int)((Math.random() * 100) + 1);
+        d = (int)((Math.random() * 100) + 1);
+        e = (a + b + c + d) / 4;
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,17 +57,18 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
 
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
@@ -70,17 +80,17 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
-
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
@@ -92,17 +102,18 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
 
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
@@ -114,17 +125,18 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
 
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
@@ -136,17 +148,18 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
 
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
@@ -158,17 +171,18 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
 
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
@@ -180,17 +194,18 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
 
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
@@ -202,17 +217,18 @@ public class MainActivity extends AppCompatActivity {
                 int blue = 0;
                 int green = 0;
 
-                red = (int)(Math.random()*255);
-                blue = (int)(Math.random()*255);
-                green = (int)(Math.random()*255);
+                red = (int)(Math.random() * 256);
+                blue = (int)(Math.random() * 256);
+                green = (int)(Math.random() * 256);
 
-                vFlipper.setBackgroundColor(Color.rgb(red, green, blue));
+                linearLayout.setBackgroundColor(Color.rgb(red, green, blue));
 
 
-                text1.setText((int)(Math.random() * 100) + "점");
-                text2.setText((int)(Math.random() * 100) + "점");
-                text3.setText((int)(Math.random() * 100) + "점");
-                text4.setText((int)(Math.random() * 100) + "점");
+                text1.setText(a + "점");
+                text2.setText(b + "점");
+                text3.setText(c + "점");
+                text4.setText(d + "점");
+                text5.setText(e + "점");
 
             }
         });
